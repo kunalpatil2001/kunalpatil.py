3469231ef418811db04448620ee71eb1d6c97cad
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2021-11-19 00:03:22 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,24 @@
+Write a Python program to parse a string to Float & Integer\n"
+"a = \"2000\"\n",
+        "b = int(a)\n",
+        "c = float(a)\n",
+        "\n",
+        "print(type(b))\n",
+        "print(b)\n",
+        "\n",
+        "print(type(c))\n",
+        "print(c)"
+      ],
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "<class 'int'>\n",
+            "2000\n",
+            "<class 'float'>\n",
+            "2000.0\n"
+          ]
+        }
+      ]
