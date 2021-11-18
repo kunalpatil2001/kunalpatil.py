a70be3f3fa15f397c7d04126b9e4672d58d23195
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2021-11-18 16:29:41 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,20 @@
+Accept two numbers from the user and multiply them together.\n",
+        "val1=int(input(\"enter no. : \"))\n",
+        "val2=int(input(\"enter no. : \"))\n",
+        "def prod(x,y):\n",
+        "  return x*y\n",
+        "print(prod(val1,val2))\n"
+      ],
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter no. : 8\n",
+            "enter no. : 9\n",
+            "11\n"
+          ]
+        }
+      ]
+    },
