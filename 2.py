2dfa07bd7b6afce2f223ce699f52fd524d5a8076
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2021-11-18 16:24:46 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,6 @@
+Write a Python program to get the volume of a sphere with radius 10.\n",
+pi = 3.1415926535897931\n",
+r= 10\n",
+V= 4.0/3.0*pi* r**3\n",
+print('The volume of the sphere is: ',V)"
+],
