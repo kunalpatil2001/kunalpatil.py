38a6502dac1133c5e9ce6e53b4849a11607a8ef9
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2021-11-19 13:32:57 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,23 @@
+A shop will give discount of 50% if the cost of purchased quantity is more than 10000. Ask user for quantity suppose, one unit will cost 100. Judge and print total cost for user.\n",
+"quantity=int(input(\"enter quantity of item :\"))\n",
+        "price=quantity*100\n",
+        "if(price>10000):\n",
+        "  print(\"total price is :\",(price/100)*50)\n",
+        "else:\n",
+        "  print(\"total price is :\",price)"
+      ],
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter quantity of item :101\n",
+            "total price is : 5050.0\n"
+          ]
+        }
+      ]
+    },
+    
+    
+    output:-total price is : 5050.0\n"
