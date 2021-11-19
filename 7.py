18e4a5b5cd87fc5112af22babd6d4014efb934c1
kunalpatil2001 <92154752+kunalpatil2001@users.noreply.github.com>
2021-11-19 13:11:59 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,15 @@
+Given two integer numbers return their sum. If the sum is greater than 100, then return their product.\n",
+val1=5\n",
+val279\n",
+"def prod(x,y):\n",
+"  return x*y\n",
+"def sum(x,y):\n",
+"  return x+y\n",
+"sum=sum(val1,val2)\n",
+"if(sum>100):\n",
+"  print(\"product :\",prod(val1,val2))\n",
+"else:\n",
+"  print(\"sum :\",sum)"
+],
+      
+      
